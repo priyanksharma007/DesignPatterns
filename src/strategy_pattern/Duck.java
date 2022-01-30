a0c065f3929eae94c49cad509f9d77d4aa011ca2
priyank <connect.priyanksharma@gmail.com>
2022-01-30 22:36:23 +0530

    add strategy_design_pattern

---
+++ src/strategy_pattern/Duck.java
@@ -0,0 +1,20 @@
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    
+    public Duck() { }
+    
+    public void performFly() {
+        
+    }
+    
+    public void performQuack() {
+        
+    }
+    
+    public abstract void display();
+    
+    public void swim() {
+        System.out.println("All ducks float, even decoys");
+    }
+}
---
+++ src/strategy_pattern/MiniDuckSimulator.java
@@ -0,0 +1,4 @@
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+    }
+}
